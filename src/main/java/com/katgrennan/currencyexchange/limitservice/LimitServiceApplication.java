d9aa35f1352a45df2a373fc073d3d5de5af959b7
nunya biz <nunya@bizniz.con>
2020-09-20 21:45:59 -0400

    Add Eureka Client

--- src/main/java/com/katgrennan/currencyexchange/limitservice/LimitServiceApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/limitservice/LimitServiceApplication.java
@@ -2,9 +2,12 @@ package com.katgrennan.currencyexchange.limitservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // Simple instance limiting service
 @SpringBootApplication
+// Enable discovery for Eureka naming server
+@EnableDiscoveryClient
 public class LimitServiceApplication {
 
 	public static void main(String[] args) {
