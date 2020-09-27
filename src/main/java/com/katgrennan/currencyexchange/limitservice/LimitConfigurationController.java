83d16abf06ef96a68ca5e284b2013f5a787f0f19
nunya biz <nunya@bizniz.con>
2020-09-27 11:48:31 -0400

    Add fallback method for getting limits

--- src/main/java/com/katgrennan/currencyexchange/limitservice/LimitConfigurationController.java
+++ src/main/java/com/katgrennan/currencyexchange/limitservice/LimitConfigurationController.java
@@ -1,6 +1,7 @@
 package com.katgrennan.currencyexchange.limitservice;
 
 import com.katgrennan.currencyexchange.limitservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,17 @@ public class LimitConfigurationController {
 
     // GET endpoint to get instance limits
     @GetMapping("/limits")
+    @HystrixCommand(fallbackMethod = "fallbackGetLimitsFromConfig")
     public LimitConfiguration getLimitsFromConfig() {
+        LimitConfiguration config = new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+        if (config.getMaximum() == 0) {
+            throw new RuntimeException("LimitService: Spring Cloud Config unavailable - Falling back on default limits");
+        }
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    public LimitConfiguration fallbackGetLimitsFromConfig() {
+        return new LimitConfiguration(1, 999);
     }
 
 }
