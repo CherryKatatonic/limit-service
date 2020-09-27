23979599c4ff611a2910cff116761f033d51c908
nunya biz <nunya@bizniz.con>
2020-09-27 11:47:24 -0400

    Enable Hystrix for fault tolerance

--- src/main/java/com/katgrennan/currencyexchange/limitservice/LimitServiceApplication.java
+++ src/main/java/com/katgrennan/currencyexchange/limitservice/LimitServiceApplication.java
@@ -3,11 +3,14 @@ package com.katgrennan.currencyexchange.limitservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 // Simple instance limiting service
 @SpringBootApplication
 // Enable discovery for Eureka naming server
 @EnableDiscoveryClient
+// Enable Hystrix for fault tolerance
+@EnableHystrix
 public class LimitServiceApplication {
 
 	public static void main(String[] args) {
